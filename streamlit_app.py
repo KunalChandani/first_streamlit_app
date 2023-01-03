d84c96739bd3522e6bc0c303763d60921c1861e9
KunalChandani <33264093+KunalChandani@users.noreply.github.com>
2023-01-03 15:13:38 +0100

    Update streamlit_app.py
    Just playing around

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit  
 
 streamlit.title("My parents new healthy dinner")
+streamlit.header("Breakfast menu")
+streamlit.text("My parents new healthy dinner")
+streamlit.text("My parents new healthy dinner")
